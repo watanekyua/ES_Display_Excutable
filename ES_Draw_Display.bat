a1e036d4383a391785e61ebf4ab1b35129ccf2af
Watane Kyua <99834141+watanekyua@users.noreply.github.com>
2024-12-19 17:01:02 +0800

    動態廣告時間

--- ES_Draw_Display.bat
+++ ES_Draw_Display.bat
@@ -2,5 +2,5 @@
 echo !!!
 echo Wait for system prepare...
 ping 127.0.0.1 -n 10 -w 1000
-cd /D C:\Users\user\Documents\GitHub\ES_Display_Excutable
+cd /D C:\Users\User\Documents\GitHub\_Build\ES_Display
 ES_Draw_Display.exe -screen-fullscreen 1
